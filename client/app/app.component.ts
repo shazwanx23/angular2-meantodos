7e01ca116c521e7c65118466457259981dabe699
shazwan <shazwanx23@gmail.com>
2017-01-31 15:02:18 +0800

    finish mean todos app

---
+++ client/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TodoService } from './services/todo.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  providers: [TodoService]
+})
+export class AppComponent  { name = 'Angular'; }
